23f678e041cdc4943d7d23bfdeed626025227279
chkas <christof.kaser@gmail.com>
2024-07-19 09:51:22 +0200

    mc

---
+++ misc/ros/Monte_Carlo_methods.el
@@ -0,0 +1,15 @@
+func mc n .
+   for i = 1 to n
+      x = randomf
+      y = randomf
+      if x * x + y * y < 1
+         hit += 1
+      .
+   .
+   return 4 * hit / n
+.
+numfmt 4 0
+print mc 10000
+print mc 100000
+print mc 1000000
+print mc 10000000
